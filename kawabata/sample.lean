68e4bafa01bf780ddae7a9e1147b5d37a98be800
Akiyuki1525 <kwbt0617@gmail.com>
2023-10-13 10:23:58 +0900

    10/12更新(川端)

--- kawabata/sample.lean
+++ kawabata/sample.lean
@@ -4,4 +4,68 @@ import Mathlib
 みてください. sorry の部分は書き直してください
 
 書き込みました
+<<<<<<< Updated upstream
  -/
+=======
+ -/
+
+ 
+theorem exists_prime_factor {n : ℕ } (h : 2 ≤ n) :
+ ∃ p : ℕ , Nat.Prime p ∧ p ∣ n := by
+
+  by_cases np : Nat.Prime n
+  {use n}
+  induction' n using Nat.strong_induction_on with n ih 
+  --何故P(0)出ない？--
+  rw [Nat.prime_def_lt] at np
+  push_neg at np
+  rcases np h with ⟨m, mltn, mdvdn, mne1⟩
+  have mne0: m ≠ 0
+  { intro mz
+    rw [mz, zero_dvd_iff] at mdvdn
+    linarith }
+  have mgt2 : 2 ≤ m
+  {sorry} --two_le this mne1--
+  by_cases mp : Nat.Prime m
+  exists m
+  {rcases ih m mltn mgt2 mp with ⟨p, pp, pdvd⟩
+   exists p
+   refine ⟨ pp, pdvd.trans mdvdn⟩}
+
+
+theorem primes_infinite : ∀ n, ∃ p > n, Nat.Prime p := by
+ intro n
+  
+ have h : 2 ≤ Nat.factorial (n + 1) + 1
+ { apply Nat.succ_le_succ
+   exact Nat.succ_le_of_lt (Nat.factorial_pos _)}
+ rcases exists_prime_factor h with ⟨p, pp, pdvd⟩
+ exists p   --refine で何故かnogoal--
+ constructor
+ by_contra ple
+ push_neg at ple
+ have : p ∣ Nat.factorial (n + 1)
+ {apply Nat.dvd_factorial
+  apply pp.pos
+  linarith}
+ have pdvd1 : p ∣ 1
+ {convert Nat.dvd_sub' pdvd this
+  simp}
+ have ple1 : p ≤ 1
+ {apply Nat.le_of_dvd
+  apply zero_lt_one
+  exact pdvd1 }
+ linarith [pp.two_le]
+ exact pp
+
+
+
+theorem e {n : ℕ }  : n ∣ n := by
+ apply?
+
+theorem two : ∀ {m : ℕ}, m ≠ 0 → m ≠ 1 → 2 ≤ m := by
+ apply?
+
+theorem dt {a b c : ℕ } : a ∣ b → b ∣ c → a ∣ c := by
+ exact Nat.dvd_trans
+>>>>>>> Stashed changes
