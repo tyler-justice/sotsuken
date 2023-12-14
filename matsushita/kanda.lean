4e8d164d6386f89bb819d952458ae1664312da4e
Daisuke <matusita@math.sci.hokudai.ac.jp>
2023-12-15 05:04:05 +0900

    reduce と eval の違いを確認した

--- matsushita/kanda.lean
+++ matsushita/kanda.lean
@@ -6,3 +6,39 @@ import Mathlib
   ください. Tutorial の 4 節が参考になります.
 -/
 
+/- Lean の reduce と eval の違いを確かめるサンプルプログラム-/
+
+def f  (n : Nat) := n
+
+def g  (n : Nat) := 1*n
+
+
+theorem f_eq_g : f = g := by
+  funext
+  unfold f
+  unfold g
+  rw [one_mul]
+
+--  funext fun n => (Nat.zero_add n).symm
+
+
+def val : Nat :=
+  Eq.recOn (motive := fun _ _ => Nat) f_eq_g 0
+
+#reduce val
+
+#eval val
+
+theorem tteq : (True ∧ True) = True :=
+  propext (Iff.intro (fun ⟨h, _⟩ => h) (fun h => ⟨h, h⟩))
+
+def val1 : Nat :=
+  Eq.recOn (motive := fun _ _ => Nat) tteq 0
+
+-- does not reduce to 0
+#reduce val1
+
+-- evaluates to 0
+#eval val1
+
+#eval Real.sin 2
